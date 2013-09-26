839d515d8638139cf08e9cdf11bc00f5fe66d5a3
shadibarhoumi <shadibarhoumi@gmail.com>
2013-09-26 01:37:42 -0700

    finished shitty fixes

---
+++ js/site.js
@@ -0,0 +1,7 @@
+// $(function() {
+// 	console.log('hi');
+// 	$('.faq-question').click(function(e) {
+// 		console.log('clicked faq-question');
+// 		$(e.target).next().removeClass('hidden');
+// 	});
+// });
